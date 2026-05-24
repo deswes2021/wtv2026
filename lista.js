3724c490ff940b0b267c97cbe15a0ee99e1dde63
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2026-05-24 15:48:23 -0600

    Update lista.js

--- lista.js
+++ lista.js
@@ -5,121 +5,121 @@
 {
   nombre:"opciones",
   tipo:"menu",
-  logo:"./img/opciones.jpg",
+  logo:"opciones.jpg",
   url:"opciones"
 },
 {
   nombre:"nacionales",
   tipo:"menu",
-  logo:"./img/nacionales.jpg",
+  logo:"nacionales.jpg",
   url:"nacionales"
 },
 {
   nombre:"infantiles",
   tipo:"menu",
-  logo:"./img/infantiles.jpg",
+  logo:"infantiles.jpg",
   url:"infantiles"
 },
 {
   nombre:"novelas",
   tipo:"menu",
-  logo:"./img/novelas.jpg",
+  logo:"novelas.jpg",
   url:"novelas"
 },
 {
   nombre:"naturaleza",
   tipo:"menu",
-  logo:"./img/naturaleza.jpg",
+  logo:"naturaleza.jpg",
   url:"naturaleza"
 },
 {
   nombre:"variedades",
   tipo:"menu",
-  logo:"./img/variedades.jpg",
+  logo:"variedades.jpg",
   url:"variedades"
 },
 {
   nombre:"cinemas",
   tipo:"menu",
-  logo:"./img/cinemas.jpg",
+  logo:"cinemas.jpg",
   url:"cinemas"
 },
 {
   nombre:"deportes",
   tipo:"menu",
-  logo:"./img/deportes.jpg",
+  logo:"deportes.jpg",
   url:"deportes"
 },
 {
   nombre:"religiosos",
   tipo:"menu",
-  logo:"./img/religiosos.jpg",
+  logo:"religiosos.jpg",
   url:"religiosos"
 },
 {
   nombre:"musicales",
   tipo:"menu",
-  logo:"./img/musicales.jpg",
+  logo:"musicales.jpg",
   url:"musicales"
 },
 {
   nombre:"YTB Oeste",
   tipo:"menu",
-  logo:"./img/ytboeste.jpg",
+  logo:"ytboeste.jpg",
   url:"ytboeste"
 },
 {
   nombre:"YTB Guerra",
   tipo:"menu",
-  logo:"./img/ytbguerra.jpg",
+  logo:"ytbguerra.jpg",
   url:"ytbguerra"
 },
 {
   nombre:"YTB Ficcion",
   tipo:"menu",
-  logo:"./img/ytbficcion.jpg",
+  logo:"ytbficcion.jpg",
   url:"ytbficcion"
 },
 {
   nombre:"YTB Fantasia",
   tipo:"menu",
-  logo:"./img/ytbfantasia.jpg",
+  logo:"ytbfantasia.jpg",
   url:"ytbfantasia"
 },
 {
   nombre:"YTB Horror",
   tipo:"menu",
-  logo:"./img/ytbhorror.jpg",
+  logo:"ytbhorror.jpg",
   url:"ytbhorror"
 },
 {
   nombre:"YTB Infantil",
   tipo:"menu",
-  logo:"./img/ytbinfantil.jpg",
+  logo:"ytbinfantil.jpg",
   url:"ytbinfantil"
 },
 {
   nombre:"YTB Anime",
   tipo:"menu",
-  logo:"./img/ytbanime.jpg",
+  logo:"ytbanime.jpg",
   url:"ytbanime"
 },
 {
   nombre:"YTB Familiar",
   tipo:"menu",
-  logo:"./img/ytbfamiliar.jpg",
+  logo:"ytbfamiliar.jpg",
   url:"ytbfamiliar"
 },
 {
   nombre:"YTB Musicales",
   tipo:"menu",
-  logo:"./img/ytbmusicales.jpg",
+  logo:"ytbmusicales.jpg",
   url:"ytbmusicales"
 },
 {
   nombre:"adultos",
   tipo:"menu",
-  logo:"./img/adultos.jpg",
+  logo:"adultos.jpg",
   url:"adultos"
 },
 
@@ -127,13 +127,13 @@
   {
   nombre:"actualizar",
   tipo:"opciones",
-  logo:"./img/actualizar.jpg",
+  logo:"actualizar.jpg",
   url:"actualizar"
 },
   {
   nombre:"borrar cache",
   tipo:"opciones",
-  logo:"./img/borrarcache.jpg",
+  logo:"borrarcache.jpg",
   url:"borrarcache"
 }
  // ====== youtube ======
