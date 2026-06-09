dac9087b73079d18427d3ffd3a2b8d74188e7b96
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2026-06-08 23:51:01 -0600

    Update scr2026.js

--- scr2026.js
+++ scr2026.js
@@ -1,8 +1,14 @@
-window.wtv = { 
-nombre: "deswes";
-version: "1.0";
-añp: "2026";
+window.wtv = {
+nom: "deswes",
+ver: "1.0",
+año: "2026",
 
-chkCANAL() { alert("uno"); }
-setCANAL() { alert("dos"); }
+setMENU() {
+  $(`body`).empty();
+  $(`<div id="XHtml">`).appendTo(`body`);
+  $(`<div id="XBody">`).appendTo(`#XHtml`);
+},
+  
+setPLAYER() { alert(`777`); }
+  
 }
