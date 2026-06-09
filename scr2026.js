ef27535105de94335fcf457adab115252d83aadb
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2026-06-08 23:33:47 -0600

    Update scr2026.js

--- scr2026.js
+++ scr2026.js
@@ -1,6 +1,8 @@
-let nombre: "deswes";
-let version: "1.0";
-let añp: "2026";
+window.wtv = { 
+nombre: "deswes";
+version: "1.0";
+añp: "2026";
 
-function chkCANAL() { alert("uno"); }
-function setCANAL() { alert("dos"); }
+chkCANAL() { alert("uno"); }
+setCANAL() { alert("dos"); }
+}
